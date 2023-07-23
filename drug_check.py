33383f78bc09e75b7d7d0029c8cf14e58236a4b1
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2023-07-23 21:28:32 +0530

    added post method

---
+++ drug_check.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+import json
+
+app = Flask(__name__)
+
+@app.route('/check_drug_formation', methods=['POST'])
+def check_drug_formation():
+    data = json.loads(request.data)
+    loaded_model = pickle.load(open('finalized_model.pkl', 'rb'))
+    dp = np.array(list(data.values())).reshape(1,-1)
+    result = loaded_model.predict(dp)
+    # result=1
+    res = ""
+    if result==1:
+        res = "yes"
+    else:
+        res = "no"
+    return res
+
+if __name__ == '__main__':
+    app.run(debug=True)
